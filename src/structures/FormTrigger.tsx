0f00c26d535a197e377f25748459cb7d7c3f32ff
Divyam Anand <wrkdivan@gmail.com>
2025-04-16 07:55:06 +0530

    Formtrigger fix

--- src/structures/FormTrigger.tsx
+++ src/structures/FormTrigger.tsx
@@ -1,15 +1,13 @@
-import { Button } from '@/components/ui/button'
 import { PlusCircle } from 'lucide-react'
 
 const FormTrigger = () => {
   return (
-    <Button
-      variant="default"
-      className="flex items-center gap-2 px-4 py-2 rounded-md"
+    <div
+      className="flex items-center gap-2 px-4 py-2 rounded-md bg-primary text-background font-extralight"
     >
       <PlusCircle size={20} />
       <span className="text-sm font-medium">Add Question</span>
-    </Button>
+    </div>
   )
 }
 
